2b4cc47990925605c36933ecc70109475e1250fb
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2022-08-17 18:16:50 -0600

    Replaced Helper.cs

--- Karty/Components/Helper.cs
+++ Karty/Components/Helper.cs
@@ -8,28 +8,28 @@ using UNO.Cards;
 namespace UNO.Components
 {
     /// <summary>
-    /// Pomocná třída. Obsahuje to, co se jinam nehodilo.
+    /// Helper class. It contains what did not fit elsewhere.
     /// </summary>
     public class Helper
     {
         /// <summary>
-        /// Porovná dvě karty. Vrátí true, pokud sou navzájem na sebe hratelné.
+        /// Compares two cards. Returns true if they are playable on top of each other.
         /// </summary>
         public static bool CompareCards(Card c1, Card c2)
         {
-            // Porovnání barev
+            // Color comparison
             if (c1.Color == c2.Color)
             {
                 return true;
             }
 
-            // Porovnání čísel
+            // Comparing numbers
             if (c1.Number == c2.Number)
             {
                 return true;
             }
 
-            // Karty nemají nic společného, vrátí false
+            // Cards have nothing to do with it, it returns false
             return false;
         }
     }
