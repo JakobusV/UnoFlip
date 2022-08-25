1b59d19d24f86fe650a52a1f8b9cd915500162cd
juheathershaw <jheathershaw@student.neumont.edu>
2022-08-24 20:06:18 -0600

    unit test class and skeletons of test methods created, and helper class check for wild cards

---
+++ UnoTesting/UnoTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UNO;
+using System;
+
+namespace UnoTesting
+{
+    [TestClass]
+    public class UnoTests
+    {
+        //shuffle, create, mouseClick, checkWin, card abiities 
+
+        [TestMethod]
+        public void PlayerWinTest()
+        {
+            
+        }
+        [TestMethod]
+        public void ShuffleCardsTest()
+        {
+            
+        }
+        [TestMethod]
+        public void CreateGameTest()
+        {
+
+        }
+        [TestMethod]
+        public void mouseClickTestTest()
+        {
+
+        }
+        [TestMethod]
+        public void SkipTest()
+        {
+
+        }
+        [TestMethod]
+        public void reverseTest()
+        {
+
+        }
+        [TestMethod]
+        public void skipAllTest()
+        {
+
+        }
+        [TestMethod]
+        public void flipTest()
+        {
+
+        }
+        [TestMethod]
+        public void plusOneTest()
+        {
+
+        }
+        [TestMethod]
+        public void plusFiveTest()
+        {
+
+        }
+        [TestMethod]
+        public void wildTest()
+        {
+
+        }
+        [TestMethod]
+        public void wildDtawTwoTest()
+        {
+
+        }
+        [TestMethod]
+        public void wildDrawTillMatchTest()
+        {
+
+        }
+    }
+}
