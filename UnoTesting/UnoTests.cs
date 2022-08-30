d848035974d063e249d27482e895233e876efc8d
juheathershaw <jheathershaw@student.neumont.edu>
2022-08-29 20:12:02 -0600

    updated unit tests. some functioning

--- UnoTesting/UnoTests.cs
+++ UnoTesting/UnoTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UNO;
 using System;
+using Karty.Components;
+using System.Drawing;
 
 namespace UnoTesting
 {
@@ -12,7 +14,7 @@ namespace UnoTesting
         [TestMethod]
         public void PlayerWinTest()
         {
-            
+
         }
         [TestMethod]
         public void ShuffleCardsTest()
@@ -22,57 +24,86 @@ namespace UnoTesting
         [TestMethod]
         public void CreateGameTest()
         {
-
+            
         }
         [TestMethod]
         public void mouseClickTestTest()
         {
-
+            
         }
         [TestMethod]
         public void SkipTest()
         {
-
+            /*var expexted = Player.canPlay;
+            var actual = !Player.canPlay;
+            Assert.AreNotEqual(expexted, actual);*/
+            
         }
         [TestMethod]
         public void reverseTest()
         {
-
+            
         }
         [TestMethod]
         public void skipAllTest()
         {
-
+            
         }
         [TestMethod]
         public void flipTest()
         {
-
+            
         }
         [TestMethod]
         public void plusOneTest()
         {
-
+            CardAbilities cardAbilities = new CardAbilities();
+            var expexted = Player.hand.Count == 8;
+            var actual = Player.hand.Count;
+            Player.Draw(1);
+            cardAbilities.DrawOneAbility();
+            Assert.AreEqual(expexted, actual);
         }
         [TestMethod]
         public void plusFiveTest()
         {
-
+            CardAbilities cardAbilities = new CardAbilities();
+            var expexted = Player.hand.Count == 12;
+            var actual = Player.hand.Count;
+            Player.Draw(5);
+            cardAbilities.DrawOneAbility();
+            Assert.AreEqual(expexted, actual);
         }
         [TestMethod]
         public void wildTest()
         {
-
+            CardAbilities cardAbilities = new CardAbilities();
+            cardAbilities.WildAbility(Color.Red);
+            var expected = Color.Red;
+            var actual = Pile.cards[Pile.cards.Count - 1].Color;
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void wildDtawTwoTest()
+        public void wildDrawTwoTest()
         {
-
+            var expectedHand = Player.hand.Count == 9;
+            Player.Draw(2);
+            var actualHand = Player.hand.Count;
+            CardAbilities cardAbilities = new CardAbilities();
+            cardAbilities.WildDrawTwoAbility(Color.Red);
+            var expectedColor = Color.Red;
+            var actualColor = Pile.cards[Pile.cards.Count - 1].Color;
+            Assert.AreEqual(expectedHand, actualHand);
+            Assert.AreEqual(expectedColor, actualColor);
         }
         [TestMethod]
         public void wildDrawTillMatchTest()
         {
-
+            CardAbilities cardAbilities = new CardAbilities();
+            cardAbilities.WildAbility(Color.Red);
+            var expected = Color.Red;
+            var actual = Pile.cards[Pile.cards.Count - 1].Color;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
